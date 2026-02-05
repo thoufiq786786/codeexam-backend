bc07c3e367e50d8107dcd2af6f98e852788b16da
Thoufiq <2223029@saec.ac.in>
2026-02-05 15:24:55 +0530

    Update base url

--- main.py
+++ main.py
@@ -14,9 +14,7 @@ app = FastAPI()
 # FIX: Ensure CORSMiddleware is added immediately after app initialization
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [
-    "https://codeexam-frontend.vercel.app",
-    "http://localhost:5173"],  # Allows all origins
+    allow_origins = ["https://codeexam-frontend.vercel.app"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], # Explicitly include OPTIONS
     allow_headers=["*"],
